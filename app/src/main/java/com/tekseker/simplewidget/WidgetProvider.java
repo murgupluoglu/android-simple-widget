4e2c1e9f18970933379785e43442d370933afe59
mustafa.urgupluoglu <mustafa.urgupluoglu@cellsigma.com>
2017-10-03 18:09:57 +0300

    init

---
+++ app/src/main/java/com/tekseker/simplewidget/WidgetProvider.java
@@ -0,0 +1,96 @@
+package com.tekseker.simplewidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.Gravity;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+
+/**
+ * Created by mustafa.urgupluoglu on 02/10/17.
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    private static final String LIST_ITEM_CLICKED_ACTION = "LIST_ITEM_CLICKED_ACTION";
+    private static final String REFRESH_WIDGET_ACTION = "REFRESH_WIDGET_ACTION";
+    public static final String EXTRA_CLICKED_FILE = "EXTRA_CLICKED_FILE";
+
+
+    /**
+     * "This is called to update the App Widget at intervals defined by the updatePeriodMillis attribute in the
+     * AppWidgetProviderInfo (see Adding the AppWidgetProviderInfo Metadata above). This method is also called when the
+     * user adds the App Widget, so it should perform the essential setup, such as define event handlers for Views and
+     * start a temporary Service, if necessary. However, if you have declared a configuration Activity, this method is
+     * not called when the user adds the App Widget, but is called for the subsequent updates. It is the responsibility
+     * of the configuration Activity to perform the first update when configuration is done. (See Creating an App Widget
+     * Configuration Activity below.)"
+     */
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+            // The empty view is displayed when the collection has no items. It should be a sibling
+            // of the collection view:
+            rv.setEmptyView(R.id.widget_list, R.id.empty_view);
+
+            // Setup intent which points to the WidgetService which will provide the views for this collection.
+            Intent intent = new Intent(context, WidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            // When intents are compared, the extras are ignored, so we need to embed the extras
+            // into the data so that the extras will not be ignored.
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            rv.setRemoteAdapter(R.id.widget_list, intent);
+
+            // Setup refresh button:
+            Intent refreshIntent = new Intent(context, WidgetProvider.class);
+            refreshIntent.setAction(WidgetProvider.REFRESH_WIDGET_ACTION);
+            refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            refreshIntent.setData(Uri.parse(refreshIntent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setOnClickPendingIntent(R.id.refresh_button, refreshPendingIntent);
+
+            // Here we setup the a pending intent template. Individuals items of a collection
+            // cannot setup their own pending intents, instead, the collection as a whole can
+            // setup a pending intent template, and the individual items can set a fillInIntent
+            // to create unique before on an item to item basis.
+            Intent toastIntent = new Intent(context, WidgetProvider.class);
+            toastIntent.setAction(WidgetProvider.LIST_ITEM_CLICKED_ACTION);
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setPendingIntentTemplate(R.id.widget_list, toastPendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, rv);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        switch (intent.getAction()) {
+            case LIST_ITEM_CLICKED_ACTION:
+                String clickedFilePath = intent.getStringExtra(EXTRA_CLICKED_FILE);
+                Toast toast = Toast.makeText(context, "LIST_ITEM_CLICKED_ACTION: " + clickedFilePath, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                break;
+            case REFRESH_WIDGET_ACTION:
+                int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+                AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
+
+                Toast toast2 = Toast.makeText(context, "REFRESH_WIDGET_ACTION", Toast.LENGTH_SHORT);
+                toast2.setGravity(Gravity.CENTER, 0, 0);
+                toast2.show();
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/tekseker/simplewidget/WidgetService.java
@@ -0,0 +1,125 @@
+package com.tekseker.simplewidget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by mustafa.urgupluoglu on 02/10/17.
+ */
+
+public final class WidgetService extends RemoteViewsService {
+
+
+    public static final class WidgetItem {
+
+        /** Label to display in the list. */
+        public final String mLabel;
+        /** The file which this item represents, sent with the {@link WidgetProvider#EXTRA_CLICKED_FILE} extra. */
+        public final String mFile;
+
+        public WidgetItem(String mLabel, String mFile) {
+            this.mLabel = mLabel;
+            this.mFile = mFile;
+        }
+    }
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new ListRemoteViewsFactory(getApplicationContext());
+    }
+
+    public static class ListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+        private final List<WidgetItem> mWidgetItems = new ArrayList<>();
+        private final Context mContext;
+
+        public ListRemoteViewsFactory(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public void onCreate() {
+            // In onCreate() you setup any connections / cursors to your data source. Heavy lifting,
+            // for example downloading or creating content etc, should be deferred to onDataSetChanged()
+            // or getViewAt(). Taking more than 20 seconds in this call will result in an ANR.
+
+
+        }
+
+        @Override
+        public void onDestroy() {
+            mWidgetItems.clear();
+        }
+
+        @Override
+        public int getCount() {
+            return mWidgetItems.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            // Position will always range from 0 to getCount() - 1.
+            WidgetItem widgetItem = mWidgetItems.get(position);
+
+            // Construct remote views item based on the item xml file and set text based on position.
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
+            rv.setTextViewText(R.id.widget_item, widgetItem.mLabel);
+
+            // Next, we set a fill-intent which will be used to fill-in the pending intent template
+            // which is set on the collection view in AppWidgetProvider.
+            Intent fillInIntent = new Intent().putExtra(WidgetProvider.EXTRA_CLICKED_FILE, widgetItem.mFile);
+            rv.setOnClickFillInIntent(R.id.widget_item_layout, fillInIntent);
+
+            // You can do heaving lifting in here, synchronously. For example, if you need to
+            // process an image, fetch something from the network, etc., it is ok to do it here,
+            // synchronously. A loading view will show up in lieu of the actual contents in the
+            // interim.
+
+            return rv;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            // You can create a custom loading view (for instance when getViewAt() is slow.) If you
+            // return null here, you will get the default loading view.
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public void onDataSetChanged() {
+            // This is triggered when you call AppWidgetManager notifyAppWidgetViewDataChanged
+            // on the collection view corresponding to this factory. You can do heaving lifting in
+            // here, synchronously. For example, if you need to process an image, fetch something
+            // from the network, etc., it is ok to do it here, synchronously. The widget will remain
+            // in its current state while work is being done here, so you don't need to worry about
+            // locking up the widget.
+            mWidgetItems.clear();
+
+            Random random = new Random();
+            for (int i = 0; i < 15; i++) {
+                WidgetItem item = new WidgetItem("mLabel: " + i + " id: " + random.nextInt(), "mFile: " + i);
+                mWidgetItems.add(item);
+            }
+        }
+    }
+}
